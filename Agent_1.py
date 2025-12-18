5771e0ff27d5fe43c39fa2c9de118645203ed79e
GopiRam <gopi.thungam.001@gmail.com>
2025-12-18 10:08:37 -0600

    Building AI agent

--- Agent_1.py
+++ Agent_1.py
@@ -1,36 +1,12 @@
-import os
 from dotenv import load_dotenv
+
+print(load_dotenv())
+
 from google import genai
 
-load_dotenv()
+gemini = genai.Client()
 
-# Client auto-reads GEMINI_API_KEY
-client = genai.Client()
+models = gemini.models.list()
 
-MODEL = "gemini-2.5-flash"  # change if needed
-
-history = []
-
-def send_message(text):
-    global history
-
-    history.append({
-        "role": "user",
-        "parts": [{"text": text}]
-    })
-
-    response = client.models.generate_content(
-        model=MODEL,
-        contents=history
-    )
-
-    history.append({
-        "role": "model",
-        "parts": [{"text": response.text}]
-    })
-
-    return response.text
-
-
-print(send_message("Hi!"))
-print(send_message("What is the capital of France?"))
+for i in models:
+    print(i.name)
