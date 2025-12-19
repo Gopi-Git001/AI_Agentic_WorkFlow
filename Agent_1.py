4919680c6698557126cdda4c0966f19a6b4fb1a8
GopiRam <gopi.thungam.001@gmail.com>
2025-12-19 07:24:59 -0600

    Building AI agent

--- Agent_1.py
+++ Agent_1.py
@@ -8,6 +8,13 @@ gemini = genai.Client()
 
 models = gemini.models.list()
 
-for i in models:
-    print(i)
+# for i in models:
+#     print(i.name)    
+import os   
 
+google_api_key = os.getenv("GOOGLE_API_KEY")
+
+if google_api_key:
+    print("Google API key is set")
+else:
+    print("Google API key is not set")
