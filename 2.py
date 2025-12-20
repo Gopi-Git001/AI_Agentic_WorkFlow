5c0611d3de1b70f4fa7e5e0d4b4f1f122975bc17
Gopi <gopi.temp@gmail.com>
2025-12-19 18:38:29 -0800

    Add some Gemini functionality

---
+++ 2.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+import os
+import google.generativeai as genai
+
+load_dotenv()
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+response = model.generate_content("Hello Gemini")
+print(response.text)
