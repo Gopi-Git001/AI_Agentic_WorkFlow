fc484f3f91de0890ba6515adba87dd311b477b4d
Gopi <gopi.temp@gmail.com>
2025-12-19 18:28:30 -0800

    Build simple interface Agent

--- 1.py
+++ 1.py
@@ -13,10 +13,8 @@ while True:
     if question.lower() in {"exit", "quit"}:
         print("Goodbye!")
         break
-
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=question
     )
-
     print("Gemini:", response.text, "\n")
