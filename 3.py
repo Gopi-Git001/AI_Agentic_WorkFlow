dd475edcbd3f9524a648873fd60836ba1bc2a5d3
GopiRam <gopi.thungam.001@gmail.com>
2025-12-22 08:20:50 -0600

    Update Gemini scripts

--- 3.py
+++ 3.py
@@ -9,4 +9,19 @@ gemini = genai.Client()
 model = gemini.models.list()
 
 for model in model:
-    print(model.name)
+     print(model.name)
+
+while True:
+
+    question = input('You: ')
+
+    if question in {'exit', 'quit'}:
+        print('Thank you')
+        break
+
+    response = gemini.models.generate_content(
+        model = "gemini-2.5-flash",
+        contents = "question"
+    )
+
+    print("Gemini:", response.text)
