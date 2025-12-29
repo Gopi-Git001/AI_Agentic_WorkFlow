c1c796b20aee46d243d19380ae4e49992d9f0b56
GopiRam <gopi.thungam.001@gmail.com>
2025-12-29 17:25:47 -0600

    Agent work flow test

--- 11.py
+++ 11.py
@@ -6,8 +6,33 @@ load_dotenv()
 
 api_key = os.getenv("OPENAI_API_KEY")
 
-if api_key:
-    print("exist")
+from openai import OpenAI
 
-else:
-    print("Nothing")
+openai = OpenAI()
+
+# message = {"role":"user","content":"what is Gravity?"}
+question = "Please propose a hard, challenging question to assess someone's IQ. Respond only with the question."
+message = [{"role":"user","content":question}]
+
+
+response = openai.chat.completions.create(
+    model = "gpt-4o-mini",
+    messages= message
+)
+
+question = response.choices[0].message.content
+
+print(question)
+
+messages = [{"role":"user","content":question}]
+
+response = openai.chat.completions.create(
+    model = "gpt-4o-mini",
+    messages= messages
+)
+
+answer = response.choices[0].message.content
+
+print(answer)
+
+
