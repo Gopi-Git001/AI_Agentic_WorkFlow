f6008bf20b0f041027df88df58f833b61c4e3fac
GopiRam <gopi.thungam.001@gmail.com>
2025-12-30 22:35:16 -0600

    Testing open AI API key work flow

---
+++ 6.py
@@ -0,0 +1,21 @@
+from dotenv import load_dotenv
+import os 
+
+load_dotenv()
+
+api_key = os.getenv("OPENAI_API_KEY")
+
+from openai import OpenAI
+
+openai = OpenAI()
+
+messages = [{'role':'user','content':'Hi! How are you?'}]
+
+response = openai.chat.completions.create(
+    model = 'gpt-4o-mini',
+    messages = messages
+)
+
+answer = response.choices[0].message.content 
+
+print(answer)
