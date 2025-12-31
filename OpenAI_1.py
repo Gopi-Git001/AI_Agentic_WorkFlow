bf73bf5cf962f1024f7c7c52dcdbb6f970a5e31f
GopiRam <gopi.thungam.001@gmail.com>
2025-12-31 09:11:08 -0600

    OpenAI Testing

--- OpenAI_1.py
+++ OpenAI_1.py
@@ -1,12 +1,12 @@
 from dotenv import load_dotenv
-from agents import Agent ,Runner,trace
+from agents import Agent ,Runner,trace,ModelSettings
 
 import asyncio
 
 load_dotenv()
 
-agent = Agent(name='Joker',instructions="You are a joker and you are funny don't repeat the same joke",model='gpt-4o-mini' )
+agent = Agent(name='Joker',instructions="You are a joker and you are funny don't repeat the same joke",model='gpt-4o-mini',model_settings=ModelSettings(temperature=1.0) )
 
-result = asyncio.run(Runner.run(agent, 'Tell me a joke'))
+result = asyncio.run(Runner.run(agent, "Tell me a joke don't repeat the same joke"))
 print(result.final_output)
     
