a5ce19dfb17937226c4bbfe44fedc7557e4931ce
Gopi Thungam <gopi.thungam.001@gmail.com>
2026-01-06 10:26:26 -0800

    Gemini API practice Test

--- Gemini.py
+++ Gemini.py
@@ -1,16 +1,16 @@
 from dotenv import load_dotenv
-import os 
+import os
 from google import genai
 
 load_dotenv()
 
-google = genai.Client(api_key = os.getenv('GEMINI_API_KEY'))
+google = genai.Client(api_key =os.getenv("GEMINI_API_KEY"))
 
 model = "gemini-2.5-flash"
 
 response = google.models.generate_content(
-    model=model,
-    contents="Hello, world!"
+    model = model,
+    contents = "Hello Gemini 2.5 Flash, how are you?"
 )
 
 print(response.text)
