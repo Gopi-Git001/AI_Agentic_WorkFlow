c2243e188c3a924c552497779196b565de7e2018
GopiRam <gopi.thungam.001@gmail.com>
2026-02-26 21:31:20 -0600

    add 22.py file

---
+++ 22.py
@@ -0,0 +1,44 @@
+import os 
+import requests
+from dotenv import load_dotenv
+load_dotenv(override=True)
+from openai import OpenAI
+api_key = os.getenv("OPENAI_API_KEY")
+
+client  = OpenAI()
+
+
+if api_key :
+    print("API KEY FOUND")    
+else:
+    print("API NOT FOUND")
+
+
+headers = {'Authorization':f"Bearer {api_key}",'content_type':"application/json"}
+
+payload = {
+    'model' : "gpt-4.1-mini",
+    'messages':[
+        {'role':'user','content':
+            "tell me a Joke"}
+    ]
+    
+}
+
+response = requests.post(
+    "https://api.openai.com/v1/chat/completions",
+    headers = headers,
+    json = payload
+)
+
+print(response.json()['choices'][0]['message']['content'])
+
+message= [{'role':'user','content':'Tell me a joke'}]
+
+responce = client.chat.completions.create(
+    model = 'gpt-4.1-mini',
+    messages= message
+    
+)
+
+print(responce.choices[0].message.content)
